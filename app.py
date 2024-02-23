d18783819ec9a9edaf839fca491a083b06f9353b
kakuPAN <155701830+kakuPAN@users.noreply.github.com>
2024-02-23 14:20:24 +0900

    Add files via upload

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
 app = Flask(__name__)
 
 @app.route("/")
@@ -7,7 +7,7 @@ def hello_world():
 
 @app.route("/two")
 def hello_two():
-    return render_template("two.html")
+    return render_template("page2.html")
 
 if __name__ == '__main__':
     app.run()
